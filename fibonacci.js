4587402d7e3c022ec66673fc4d8a9b4f3beb50d1
Mauricio Farfan <maufar402@gmail.com>
2020-12-06 16:15:41 -0600

    Primer commit

---
+++ fibonacci.js
@@ -0,0 +1,9 @@
+
+// Para efectos de prueba se usará una función en JavaScript que calcula la serie fibonacci hasta la posición indicada
+
+function fibonacci(number)
+{
+    if (number == 1 || number == 0) return 1;
+    return (fibonacci(number - 1) + fibonacci(number - 2));
+}
+
